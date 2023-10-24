b3e4477c00570a384e3c948b1ce5768d7cd36b9d
kegan <keganferdinand@gmail.com>
2023-10-24 14:23:10 -0400

    Added methods DisplayNumberedMenu, GetConvertedInput, WriteColoredLine

--- ConsoleManager/ConsoleManager.cs
+++ ConsoleManager/ConsoleManager.cs
@@ -1,14 +1,16 @@
 ﻿namespace ConsoleIOManager;
 using static System.Console;
 
-//-----------------------------V1.1-KeganF------------------------------//
+//-----------------------------V1.2-KeganF------------------------------//
 // CLASS > ConsoleManager                                               //
 //         Provides static methods for console applications             //
 //         - Display customs prompts, menus, and options                //
 //         - Handle user input                                          //
 //----------------------------------------------------------------------//
-public class ConsoleManager
+public static class ConsoleManager
 {
+    private static readonly string inputPrompt = "> ";
+    
     //----------------------------------------------------------------------//
     // METHOD > DisplayCheckBoxes                                           //          
     //          Displays the provided options in a check box format         //
@@ -53,5 +55,101 @@ public class ConsoleManager
         } while (input.Key != ConsoleKey.Enter);
 
         return selectedIndex;
+    }
+
+    //----------------------------------------------------------------------//
+    // METHOD > DisplayNumberedMenu                                         //          
+    //          Displays the provided options in a numbered menu format     //
+    //                                                                      //
+    // PARAMS > options - string array of the options to be displayed       //
+    //          prompt - optional string to be displayed above the          //
+    //                   options                                            //
+    //                                                                      //
+    // RETURN > (int) the number corresponding to the listed option         //
+    //----------------------------------------------------------------------//
+    public static int DisplayNumberedMenu(string[] options,
+        string prompt = "Select one of the following options:")
+    {
+        int input = 0;
+        
+        WriteLine(prompt);
+        // Display options in a "numbered menu" format
+        for (int i = 0; i < options.Length; i++)
+        {
+            WriteLine($"[{i+1}] - {options[i]}");
+        }
+
+        do
+        {    
+            // Get input from user; convert to int
+            input = GetConvertedInput<int>();
+            
+            // Check if input is outside of valid range
+            if (input < 1 || input > options.Length)
+            {
+                SetCursorPosition(0, CursorTop - 1);
+                WriteColoredLine(
+                    $"{input} is outside the valid range of options.", 
+                    ConsoleColor.Red);
+            }
+
+        } while (input < 1 || input > options.Length);
+        
+        return input;
+    }
+
+    // TODO - create method to display menu of options
+    //        accepts a dictionary as a param (display custom menu)
+
+    //----------------------------------------------------------------------//
+    // METHOD > GetConvertedInput                                           //          
+    //          Uses generics to convert string returned from ReadLine() to //
+    //          the desired type                                            //
+    //                                                                      //
+    // PARAMS > prompt - optional string to be displayed before the input   //
+    //                   prompt                                             //
+    //                                                                      //
+    // RETURN > (T) the value of ReadLine() converted from string -> T      //
+    //----------------------------------------------------------------------//
+    public static T GetConvertedInput<T>(string prompt = "")
+    {
+        // Prompt the user for input
+        Write($"{prompt}{inputPrompt}");
+        try {
+            // Read input from user
+            string input = ReadLine() ?? "";
+            // Attempt to convert input string to desired type...
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+        catch (Exception ex)
+        {
+            // ...re-prompt for input if the conversion fails
+            SetCursorPosition(0, CursorTop - 1);
+            WriteColoredLine(ex.Message, ConsoleColor.Red);
+            return GetConvertedInput<T>(prompt);
+        }
+    }
+
+    //----------------------------------------------------------------------//
+    // METHOD > WriteColoredLine                                            //          
+    //          Writes the given line of text to the console in the given   //
+    //          color                                                       //
+    //                                                                      //
+    // PARAMS > text - the text to be written to the console                //
+    //          color - the foreground color to apply to the text           //
+    //                                                                      //
+    // RETURN > void                                                        //
+    //----------------------------------------------------------------------//
+    public static void WriteColoredLine(string text, ConsoleColor color)
+    {
+        // Store current color to revert to after writing colored line
+        ConsoleColor originalColor = ForegroundColor;
+
+        // Set new color; write to text to console
+        ForegroundColor = color;
+        WriteLine(text);
+        
+        // Revert to original foreground color
+        ForegroundColor = originalColor;
     }
 }
--- README.md
+++ README.md
@@ -4,4 +4,9 @@ C# class library for console IO formatting and processing.
 
 ## Static Methods
 
-- **DisplayCheckBoxes** > Displays the provided options in a check box format
+| Method | Description |
+| --- | --- |
+| **DisplayCheckBoxes** | Displays the provied options in a check box format |
+| **DisplayNumberedMenu** | Displays the provided options in a numbered menu format |
+| **GetConvertedInput** | Uses generics to convert string returned from ReadLine() to the desired type |
+| **WriteColoredLine** | Writes the given line of text to the console in the given color |
